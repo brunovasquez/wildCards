6e25d503b40d11c2382aecc3397dbf3996d6107f
brunovasquez <Bruno.Vasquez@fundacion-jala.org>
2016-09-17 01:25:22 -0400

    Refactor litle changes

--- src/main/java/org/fundaciojala/org/SearchEngine.java
+++ src/main/java/org/fundaciojala/org/SearchEngine.java
@@ -10,20 +10,20 @@ import java.util.regex.Pattern;
  */
 public class SearchEngine {
 
-    public static final String REGEX = "_";
-    public static final String REPLACEMENT = "\\\\E.\\\\Q";
-    public static final String START_QUOTING = "\\Q";
-    public static final String END_QUOTING = "\\E";
+    private static final String REGEX = "_";
+    private static final String REPLACEMENT = "\\\\E.\\\\Q";
+    private static final String START_QUOTING = "\\Q";
+    private static final String END_QUOTING = "\\E";
 
     /**
      * Finds and fills a word in a sentence
      *
-     * @param word string to search.
+     * @param word     string to search.
      * @param haystack string, the sentence to find the word
      * @return return true or false
      */
     static int find(String word, String haystack) {
-        String regWord = START_QUOTING.concat(word.replaceAll(REGEX, REPLACEMENT) ).concat(END_QUOTING);
+        String regWord = START_QUOTING.concat(word.replaceAll(REGEX, REPLACEMENT)).concat(END_QUOTING);
         Matcher matcherWord = Pattern.compile(regWord).matcher(haystack);
         return matcherWord.find() ? matcherWord.start() : -1;
     }
